771d8dd2e9c6f73636b338a784f862db40bb4b9c
ralacote <rlacote1@gmail.com>
2025-12-11 18:08:37 +0100

    Développement de la classe item (Ajout du poids et de la valeur)

--- src/main/java/re/forestier/edu/rpg/Item.java
+++ src/main/java/re/forestier/edu/rpg/Item.java
@@ -3,11 +3,21 @@ package re.forestier.edu.rpg;
 public class Item {
 	private String nom;
 	private String effet;
+	private float weight;
+	private float value;
+
 	
 	public Item(String nom,String effet) {
 		this.nom = nom;
 		this.effet = effet;
 	}
+	
+	public Item(String nom,String effet,float weight,float value) {
+		this(nom,effet);
+		this.weight = weight;
+		this.value = value;
+	}
+
 	
 	public String getNom() {
 		return this.nom;
@@ -16,6 +26,15 @@ public class Item {
 	public String getEffet() {
 		return this.effet;
 	}
+	
+	public float getWeight() {
+		return this.weight;
+	}
+
+	public float getValue() {
+		return this.value;
+	}
+
 
 	
 }
