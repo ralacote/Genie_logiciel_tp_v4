33907cf5a978dbd58b67d03b731c570b99c05401
ralacote <rlacote1@gmail.com>
2025-12-11 18:09:54 +0100

    Traduction des noms en anglais pour la classe Item

--- src/main/java/re/forestier/edu/rpg/Item.java
+++ src/main/java/re/forestier/edu/rpg/Item.java
@@ -1,15 +1,15 @@
 package re.forestier.edu.rpg;
 
 public class Item {
-	private String nom;
-	private String effet;
+	private String name;
+	private String effect;
 	private float weight;
 	private float value;
 
 	
 	public Item(String nom,String effet) {
-		this.nom = nom;
-		this.effet = effet;
+		this.name = nom;
+		this.effect = effet;
 	}
 	
 	public Item(String nom,String effet,float weight,float value) {
@@ -20,11 +20,11 @@ public class Item {
 
 	
 	public String getNom() {
-		return this.nom;
+		return this.name;
 	}
 	
 	public String getEffet() {
-		return this.effet;
+		return this.effect;
 	}
 	
 	public float getWeight() {
